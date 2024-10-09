e9895d57450d936e3781cdcadbe0d49162bdbc04
Dmitry-Stukalov <dimastukalov04@gmail.com>
2024-10-09 11:04:36 +0300

    Add_Timer_cs

---
+++ Assets/Timer/Timer.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Android;
+
+public class Timer : MonoBehaviour
+{
+	public float MaxTime { get; private set; }
+	public float CurrentTime { get; private set; }
+	public bool TimerIsEnd { get; private set; }
+	public bool Pause { get; private set; }
+
+	public event Action OnTick;
+	public event Action OnTimerEnd;
+
+	public Timer(float maxtime, float startvalue = 0, bool pause = false)
+	{
+		MaxTime = maxtime;
+		CurrentTime = startvalue;
+		Pause = pause;
+
+		if (CurrentTime >= MaxTime) TimerIsEnd = true;
+	}
+
+	public void ResetTimer(bool pause)
+	{
+		TimerIsEnd = false;
+		CurrentTime = 0;
+		Pause = pause;
+	}
+
+	public void SetPause() => Pause = true;
+
+	public void Continue() => Pause = false;
+
+	private void Tick(float time)
+	{
+		if (TimerIsEnd || Pause) return;
+
+		UpdateTimer(time);
+		OnTick?.Invoke();
+	}
+
+	private void UpdateTimer(float time)
+	{
+		CurrentTime += time;
+		CurrentTime = Mathf.Clamp(CurrentTime, 0, MaxTime);
+
+		if (CurrentTime >= MaxTime)
+		{
+			TimerIsEnd = true;
+			OnTimerEnd?.Invoke();
+		}
+	}
+}
