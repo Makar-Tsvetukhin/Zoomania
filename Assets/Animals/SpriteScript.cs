d347dd9af0aab3533c84364f9082e125b18bd987
Dmitry-Stukalov <dimastukalov04@gmail.com>
2024-11-07 19:46:14 +0300

    Add Bamboo Sprites

---
+++ Assets/Animals/SpriteScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteScript : MonoBehaviour
+{
+	public Sprite Sprite {  get; set; }
+
+	public void OnAwake(Sprite sprite)
+	{
+		
+	}
+}
